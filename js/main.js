7fcbc82961fa10f84427d84833ee22d9b0a297c5
noramatech <noramatech647@gmail.com>
2025-05-29 04:11:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,90 @@
+let category_nav_list = document.querySelector(".category_nav_list");
+
+function Open_Categ_list(){
+    category_nav_list.classList.toggle("active")
+}
+
+
+
+
+
+var cart = document.querySelector(".cart");
+
+function open_close_cart() {
+    cart.classList.toggle("active")
+}
+
+
+
+
+fetch('products.json')
+.then(response => response.json())
+.then(data => {
+  
+
+     const addToCartButtons = document.querySelectorAll(".btn_add_cart")
+         
+     addToCartButtons.forEach(button => {
+          button.addEventListener("click", (event) =>{
+            const productId = event.target.getAttribute('data-id')
+            const selcetedProduct = data.find(product => product.id == productId)
+
+
+          addToCart(selcetedProduct)
+
+        
+          })
+     });
+
+})
+
+function addToCart(product) {
+   
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || []
+
+    cart.push({... product , quantity: 1})
+    localStorage.setItem('cart ' ,  JSON.stringify(cart))
+
+
+    updateCart()
+}
+
+function updateCart(){
+   const cartItemsContainer = document.getElementById("cart_items")
+
+   const cart = JSON.parse(localStorage.getItem('cart')) || []
+
+
+
+   cartItemsContainer.innerHTML = "";
+   cart.forEach((item , index) =>{
+    cartItemsContainer.innerHTML += `
+    
+          
+    <div class="item_cart">
+    <img src="${item.img}" alt="">
+
+
+    <div class="content">
+        <h4>${item.name}</h4>
+        <p class="price_cart">${item.price}</p>
+        <div class="quantity_control">
+            <button class="decrease_quantity">-</button>
+             <span class="quantity">${item.quantity}</span>
+            <button class="Increase_quantity">+</button>
+        </div>
+    </div>
+
+    <button class="delete_item"> <i class="fa-solid fa-trash-can"></i> </button>
+</div>
+
+    
+    
+    
+    
+    
+    
+    `
+   })
+}
---
+++ js/swiper.js
@@ -0,0 +1,60 @@
+
+
+// <!-- Initialize Swiper -->
+
+var swiper = new Swiper(".slider-swp ", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable:true
+    },
+    autoplay:{
+        delay:2500,
+    }
+    ,loop:true
+  });
+
+
+
+  // <!--swiper slide product-->
+
+var swiper = new Swiper(".slide_product", {
+    slidesPerView: 5,
+    spaCeBetween:20,
+    autoplay:{
+        delay:2500,
+    },
+    navigation:{
+      nextEl:".swiper-button-next",
+      prevEl:".swiper-button-prev"
+
+    },
+    loop:true,
+
+    breakpoints:{
+      1200:{
+        slidesPerView :5,
+        spaCeBetween :20 
+      },
+
+      1000:{
+        slidesPerView :4,
+        spaCeBetween :20 
+      }, 
+      
+   700:{
+        slidesPerView :3,
+        spaCeBetween :15 
+      }, 
+      
+      500:{
+        slidesPerView :2,
+        spaCeBetween :15 
+      },
+   0:{
+        slidesPerView :1,
+        spaCeBetween :10
+      },
+    }
+  });
+
